aad888ab3a46a93f2762599550a0b77dff0dee5f
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-05-31 13:55:08 +0530

    Streamlit test

--- test-streamlit.py
+++ test-streamlit.py
@@ -1,2 +1,15 @@
 import streamlit as st
 st.title("My first app")
+st.header("header")
+st.subheader('subheader')
+st.write("Welcome")
+st.text("Hi how are you")
+st.markdown("""
+# h1 tag
+## h2 tag
+### h3 tag
+:moon:<br>
+:sunglasses:
+""",True)
+d  = {1:'a',2:'b',3:'c',4:'d',5:'e'}
+st.write(d)
