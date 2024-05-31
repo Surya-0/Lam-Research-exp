186a4b7ad1aad50b2c09e814752acc1805df6a83
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-05-31 15:18:36 +0530

    Streamlit test

---
+++ data.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+a = np.array([1,2,3,4,5,6,7,8,9])
+nd = a.reshape(a)
+dic = {
+    "name":"surya",
+    "age":"20",
+    "city":"chennai"
+
+}
+
+
