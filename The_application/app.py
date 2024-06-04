614637d00279b11f9df575e570a9d99247665766
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-04 14:45:53 +0530

    Streamlit test

--- The_application/app.py
+++ The_application/app.py
@@ -1,12 +1,33 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
+from PIL import Image
+import json
+
+def load_lottie_file(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+# Load local Lottie animation
+lottie_animation = load_lottie_file("Animation - 1717478064445.json")
 
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Regression Data", "Graph Data"])
+page = st.sidebar.radio("Go to", ["Home", "Regression Data", "Graph Data"])
 
-if page == "Regression Data":
+if page == "Home":
+    st.title("Welcome to the Capacity Planning App")
+    st.write("This app helps you with capacity planning using neural networks and genetic algorithms.")
+
+    if lottie_animation:
+        st_lottie(lottie_animation, height=400, key="capacity-planning")
+
+    # Adding image example
+    image = Image.open("Capacity_Planning.png")
+    st.image(image, caption="Capacity Planning", use_column_width=True)
+
+elif page == "Regression Data":
     import regression_page
     regression_page.show()
-
 
 elif page == "Graph Data":
     import graph_page
